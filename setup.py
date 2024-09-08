df35486d66acc50cb02ed08d805c85064f56f982
himalayan-avalanche <awesome.data.creators@gmail.com>
2024-09-08 17:10:41 -0400

    updating the code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='absmlir',
     version='0.1.0',
     description='Identity Resolution Library',
-    url='https://github.com/ashwinimaurya/entity_resolution/tree/main',
+    url='https://github.com/himalayan-avalanche/entity_resolution/tree/main',
     author='Ashwini Maurya',
     author_email='akmaurya07@gmail.com',
     license="""Copyright © 2023 Ashwini Maurya. All rights reserved. .""",
